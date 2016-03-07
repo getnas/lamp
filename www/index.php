ab2d7d3f8338ae0f754165e0eebaaa683713b1ea
Herald Yu <yuhr123@gmail.com>
2016-03-08 00:00:59 +0800

    初始化项目

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>GetNAS Docker LAMP</title>
+  </head>
+  <body>
+    <h1 style="text-align:center;">Welcome to use Docker LAMP created by <a href="http://www.getnas.com" target="_blank">GetNAS.com</a></h1>
+    <div style="text-align:center;">
+      <h3>Manage MariaDB, please use <a href="./adminer.php">Adminer</a>.</h3>
+    </div>
+    <hr>
+    <?php phpinfo(); ?>
+  </body>
+</html>
